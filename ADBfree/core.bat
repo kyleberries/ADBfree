caff1770af328b2377e57c0d378eb62360754f2f
kyleberries <kyleberries@gmail.com>
2016-06-18 10:23:00 -0500

    data xfer update
    added adb pull function for redundancy

--- ADBfree/core.bat
+++ ADBfree/core.bat
@@ -48,14 +48,18 @@ ECHO ___________________________________________________________________________
 echo                                 Enable USB Debugging and plug device into USB port.
 echo                            Go to Contacts>settings>import/export and export to local storage.
 adb wait-for-device devices 1>NUL
+mkdir adbpull
+adb pull /storage/sdcard0/ adbpull/
 adb backup -noapk -f backup.ab
 ECHO ________________________________________________________________________________________________________________________  
 ECHO                 Data dump complete. Please unplug Sending device and plug in Recieving device.
 pause
 adb kill-server
-adb wait-for-device devices 1>NUL
+adb wait-for-device devices 1>NUL2
 adb restore backup.ab
+adb push ./adbpull /storage/sdcard0/
 del /f .\backup.ab
+rd /q /s adbpull
 ECHO ________________________________________________________________________________________________________________________  
 ECHO                   Don't forget to go back to contacts and IMPORT the contacts to the new phone!
 pause
